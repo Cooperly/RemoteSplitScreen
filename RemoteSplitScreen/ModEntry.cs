b0b3e9f92e0031663193df3a8b036d0d41f9b90d
void <void@freewifi.club>
2026-04-16 10:51:02 +0200

    Initial Commit

---
+++ RemoteSplitScreen/ModEntry.cs
@@ -0,0 +1,25 @@
+﻿using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+
+namespace RemoteSplitScreen;
+
+/// <summary>The mod entry point.</summary>
+internal sealed class ModEntry : Mod
+{
+	/// <summary>The mod entry point, called after the mod is first loaded.</summary>
+	public override void Entry(IModHelper helper)
+	{
+		helper.Events.Input.ButtonPressed += this.OnButtonPressed;
+	}
+
+	/// <summary>Raised after the player presses a button on the keyboard, controller, or mouse.</summary>
+	private void OnButtonPressed(object? sender, ButtonPressedEventArgs e)
+	{
+		// Load a translation string from the i18n folder and show it in a HUD message
+		var msg = Helper.Translation.Get("Message.Debug", new { name = Game1.player.Name, button = e.Button });
+
+		// print button presses to the console window
+		this.Monitor.Log(msg, LogLevel.Debug);
+	}
+}
