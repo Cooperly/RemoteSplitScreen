2ef38569448e8dd41fbd5c8ed62bdebf9e2be65e
void <void@freewifi.club>
2026-04-17 13:13:14 +0200

    Implement Harmony Patching, force-enable the start button. (TO BE REVISED)

--- RemoteSplitScreen/ModEntry.cs
+++ RemoteSplitScreen/ModEntry.cs
@@ -1,25 +1,20 @@
-﻿using StardewModdingAPI;
+﻿using HarmonyLib;
+using StardewModdingAPI;
 using StardewModdingAPI.Events;
 using StardewValley;
 
 namespace RemoteSplitScreen;
 
 /// <summary>The mod entry point.</summary>
-internal sealed class ModEntry : Mod
-{
-	/// <summary>The mod entry point, called after the mod is first loaded.</summary>
-	public override void Entry(IModHelper helper)
-	{
-		helper.Events.Input.ButtonPressed += this.OnButtonPressed;
+internal sealed class ModEntry : Mod {
+	private Harmony? harmony;
+	public override void Entry(IModHelper helper) {
+		harmony = new Harmony("club.freewifi.void.RemoteSplitScreen");
+		
+		harmony.PatchAll();
 	}
 
-	/// <summary>Raised after the player presses a button on the keyboard, controller, or mouse.</summary>
-	private void OnButtonPressed(object? sender, ButtonPressedEventArgs e)
-	{
-		// Load a translation string from the i18n folder and show it in a HUD message
-		var msg = Helper.Translation.Get("Message.Debug", new { name = Game1.player.Name, button = e.Button });
-
-		// print button presses to the console window
-		this.Monitor.Log(msg, LogLevel.Debug);
+	protected override void Dispose(bool disposing) {
+		harmony?.UnpatchAll(harmony.Id);
 	}
 }
---
+++ RemoteSplitScreen/Patches/EnableButton.cs
@@ -0,0 +1,50 @@
+using System.Reflection.Emit;
+using HarmonyLib;
+using RemoteSplitScreen.Helpers;
+using StardewValley;
+using StardewValley.Menus;
+
+namespace RemoteSplitScreen.Patches;
+
+[HarmonyPatch(typeof(OptionsPage), MethodType.Constructor,
+	new Type[] { typeof(int), typeof(int), typeof(int), typeof(int) })]
+public class EnableButton {
+	private static IEnumerable<CodeInstruction> Transpiler(
+		IEnumerable<CodeInstruction> instructions,
+		ILGenerator generator
+	) {
+		var matcher = new CodeMatcher(instructions, generator);
+
+		matcher.MatchStartForward(
+			Matches.LoadsField(typeof(Game1), nameof(Game1.game1)),
+			Matches.GetsProperty(() => default(InstanceGame)!.IsMainInstance),
+			Matches.Branches(),
+			Matches.LoadsField(typeof(Game1), nameof(Game1.game1)),
+			Matches.Calls(typeof(Game1), nameof(Game1.IsLocalCoopJoinable)),
+			Matches.Branches(),
+			Matches.LoadsConstant(0),
+			Matches.StoresLocal(0)
+		);
+
+		matcher.ThrowIfInvalid(nameof(EnableButton));
+
+		matcher.RemoveInstructions(8);
+
+		matcher.InsertAndAdvance(
+			Instructions.LoadConstant(true),
+			Instructions.StoreLocal(0)
+		);
+		
+		return matcher.Instructions();
+	}
+}
+
+// IL_0407: ldsfld       class StardewValley.Game1 StardewValley.Game1::game1
+// IL_040c: callvirt     instance bool StardewValley.InstanceGame::get_IsMainInstance()
+// IL_0411: brfalse.s    IL_041f
+
+// IL_0413: ldsfld       class StardewValley.Game1 StardewValley.Game1::game1
+// IL_0418: callvirt     instance bool StardewValley.Game1::IsLocalCoopJoinable()
+// IL_041d: br.s         IL_0420
+// IL_041f: ldc.i4.0
+// IL_0420: stloc.0      // flag
